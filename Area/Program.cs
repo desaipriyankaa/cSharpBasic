039f0ff0cdee25e05fcbd3a4a68e1b0887599155
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-03-23 09:59:52 +0530

    area of rectangle

---
+++ Area/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Area
+{
+    abstract class shape
+    {
+        public abstract int area();
+    }
+
+    class rectangle : shape
+    {
+        private int length;
+        private int width;
+
+        public rectangle(int a = 0, int b = 0)
+        {
+            length = a;
+            width = b;
+        }
+
+        public override int area()
+        {
+
+            Console.WriteLine("Rectangle area : ");
+            return (width*length);
+        }
+    }
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            rectangle r = new rectangle(20, 15);
+            double a = r.area();
+
+            Console.WriteLine("Area :{0}",a);
+            Console.ReadKey();
+        }
+    }
+}
